a570db0a8526830c33397cb5943defee4b2f1290
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-19 23:05:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,8 +23,8 @@ def word_substituter(string)
     contain = []
     contain.push(word)
     if (contain&long_words).length > 0
-      dictionary[word]
       contain = []
+      dictionary[word]
     else 
       word
     end
